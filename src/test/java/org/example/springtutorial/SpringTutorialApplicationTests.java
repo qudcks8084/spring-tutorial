1eb8d425562262eb0b006093b66e7ec1bc14957a
qudcks8084 <qudcks8084@naver.com>
2025-04-29 19:19:17 +0900

    [INIT] Spring Folder Init

---
+++ src/test/java/org/example/springtutorial/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
